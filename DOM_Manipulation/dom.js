8720c1817a33fd852fbe01e6b3c6d98f53d5f2d7
Tauriel1202 <lothlorienexplorer20@gmail.com>
2022-02-02 21:44:01 -0500

    dom manip upload

---
+++ DOM_Manipulation/dom.js
@@ -0,0 +1,23 @@
+const item = document.querySelector('#favchap');
+const lst = document.querySelector('.list');
+const btn = document.querySelector('#submit');
+btn.addEventListener('click', () => {
+  if (item.value !== ''){
+    let lstItem = document.createElement('li');
+    lstItem.innerHTML = item.value
+
+    let delBtn = document.createElement('button');
+    delBtn.innerHTML = '\u274c';
+    delBtn.addEventListener('click', (e) => {
+    let element = e.target.parentNode
+    lst.removeChild(element)
+    })
+    
+    lstItem.append(delBtn)
+    lst.append(lstItem);
+    console.log('💩')
+    item.value = ''
+    item.focus()
+    }
+  }
+);
