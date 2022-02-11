256a53c4a4720899ba669ce41a5019e32901dea5
Tauriel1202 <lothlorienexplorer20@gmail.com>
2022-02-11 17:26:03 -0500

    update

--- js/home.js
+++ js/home.js
@@ -1,5 +1,5 @@
 let last = document.querySelector('#lastMod');
 let mod = document.lastModified;
 let year = new Date().getFullYear(); 
-let lastMod = `&copy; ${year} .:|:. Courtney M. Christensen .:|:. Massachusetts<br><br>Last Updated: ${mod}`;
+let lastMod = `&copy; ${year} 🌊🌺 Courtney M. Christensen 🌊🌺 Massachusetts<br><br>Last Updated: ${mod}`;
 last.innerHTML = lastMod;
