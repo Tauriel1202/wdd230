6a0c8151f0f9f1933097d60f4dd50a46aec94a3c
Tauriel1202 <lothlorienexplorer20@gmail.com>
2022-03-09 16:40:18 -0500

    html/css fix

--- Chamber/js/directory.js
+++ Chamber/js/directory.js
@@ -1,61 +1,92 @@
-const shops = document.querySelector('.shops');
-const url = 'https://tauriel1202.github.io/wdd230/Chamber/data.json';
+const shops = document.querySelector(".shops");
+const url = "https://tauriel1202.github.io/wdd230/Chamber/data.json";
 
-fetch(url).then(function (response) {
+fetch(url)
+  .then(function (response) {
     return response.json();
   })
   .then(function (jsonObject) {
     console.table(jsonObject);
-    const cos = jsonObject['companies'];
+    const cos = jsonObject["companies"];
     cos.map(displayCos);
   });
 function displayCos(co) {
-  let singleCo = document.createElement('section')
-  let img = document.createElement('img')
-  let h2 = document.createElement('h2')
-  let p = document.createElement('p')
-  let phone = document.createElement('p')
-  let a = document.createElement('a')
+  let singleCo = document.createElement("section");
+  let img = document.createElement("img");
+  let h2 = document.createElement("h2");
+  let p = document.createElement("p");
+  let phone = document.createElement("p");
+  let a = document.createElement("a");
 
-  img.setAttribute('src', co.img)
-  img.setAttribute('alt', co.name)
-  img.setAttribute('width', 100)
-  img.setAttribute('height', 100)
+  img.setAttribute("data-src", co.img);
+  img.setAttribute("alt", co.name);
+  img.setAttribute("width", 100);
+  img.setAttribute("height", 100);
 
   h2.textContent = co.name;
   p.textContent = co.address;
   phone.textContent = co.phone;
 
   a.textContent = co.site;
-  a.setAttribute('href', '#')
+  a.setAttribute("href", "#");
 
-  singleCo.appendChild(img)
-  singleCo.appendChild(h2)
-  singleCo.appendChild(p)
-  singleCo.appendChild(phone)
-  singleCo.appendChild(a)
+  singleCo.appendChild(img);
+  singleCo.appendChild(h2);
+  singleCo.appendChild(p);
+  singleCo.appendChild(phone);
+  singleCo.appendChild(a);
 
-  shops.appendChild(singleCo)
+  shops.appendChild(singleCo);
+  lazyload();
 }
 /*************LIST MODE JS***********/
-let gridBtn = document.querySelector('#gridMode')
-let listBtn = document.querySelector('#listMode')
-let onOff = document.querySelector('#on')
+let gridBtn = document.querySelector("#gridMode");
+let listBtn = document.querySelector("#listMode");
+let onOff = document.querySelector("#on");
 
-function resize(){
-  if (window.innerWidth > 520 && window.innerWidth < 1021){
-    onOff.setAttribute('name', 'lst')
-  } 
-  else {
-    onOff.setAttribute('name', 'grid')
+function resize() {
+  if (window.innerWidth > 520 && window.innerWidth < 1021) {
+    onOff.setAttribute("name", "lst");
+  } else {
+    onOff.setAttribute("name", "grid");
   }
 }
-resize()
-window.onresize = resize
+resize();
+window.onresize = resize;
 
-listBtn.addEventListener('click', () => {
-  onOff.setAttribute('name', 'lst')
-})
-gridBtn.addEventListener('click', () => {
-  onOff.setAttribute('name', 'grid')
-})
+listBtn.addEventListener("click", () => {
+  onOff.setAttribute("name", "lst");
+});
+gridBtn.addEventListener("click", () => {
+  onOff.setAttribute("name", "grid");
+});
+
+function lazyload() {
+  let images = Array.from(document.querySelectorAll("img[data-src]"));
+  const loadImages = (image) => {
+    image.setAttribute("src", image.getAttribute("data-src"));
+    image.onload = () => {
+      image.removeAttribute("data-src");
+    };
+  };
+
+  if ("IntersectionObserver" in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+      items.map((item) => {
+        if (item.isIntersecting) {
+          loadImages(item.target);
+          observer.unobserve(item.target);
+        }
+      });
+    });
+
+    images.map((img) => {
+      observer.observe(img);
+    });
+  } else {
+    images.map((img) => {
+      load(img);
+    });
+  }
+}
+
