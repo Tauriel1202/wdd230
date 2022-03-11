4972a431291d42587ff2352dfd8555cdbaa7cedd
Tauriel1202 <lothlorienexplorer20@gmail.com>
2022-03-11 16:19:27 -0500

    weather update

--- Chamber/js/windchill.js
+++ Chamber/js/windchill.js
@@ -1,15 +1,44 @@
-let temp = parseFloat(document.querySelector("#temp").textContent);
-let speed = parseFloat(document.querySelector("#speed").textContent);
-let wc = "";
+const url =
+  "http://api.openweathermap.org/data/2.5/forecast?id=2080185&units=imperial&appid=920c6288bdbd30f49daec29c2657439c";
+const temp = document.querySelector("#temp");
+const humidity = document.querySelector("#humidity");
+const speed = document.querySelector("#speed");
+const chill = document.querySelector("#chill");
+const mainImg = document.querySelector(".mainImg");
+const weather = document.querySelector("#currentWeather");
 
-if (temp <= 50 && speed > 3) {
-  wc = windChill(temp, speed);
-  wc = `${wc}`;
-} else {
-  wc = "N/A";
-}
+fetch(url)
+  .then((response) => response.json())
+  .then((jsObject) => {
 
-document.querySelector("#chill").innerHTML = `${wc}&#8457`;
+    let weatherImg = document.createElement("img");
+    let current = jsObject.list[0].weather[0].main;
+    let t = jsObject.list[0].main.temp;
+    let h = jsObject.list[0].main.humidity;
+    let s = jsObject.list[0].wind.speed;
+    let wc = "";
+    let iconUrl = `./images/${current}.jpg`;
+    // let img = jsObject.list[0].weather[0].icon;
+    // let iconUrl = `https://openweathermap.org/img/w/${img}.png`;
+
+    weather.textContent = current;
+    temp.textContent = t.toFixed(0);
+    humidity.textContent = h.toFixed(0);
+    speed.textContent = s.toFixed(0);
+
+    weatherImg.src = iconUrl;
+    weatherImg.alt = current;
+    mainImg.append(weatherImg);
+
+    if (t <= 50 && s > 3) {
+      wc = windChill(t, s);
+      wc = `${wc} &#8457;`;
+    } else {
+      wc = "N/A";
+    }
+
+    chill.innerHTML = wc;
+  });
 
 function windChill(temp, speed) {
   wc =
@@ -20,4 +49,24 @@ function windChill(temp, speed) {
   return Math.round(wc);
 }
 
-console.log(wc);
+// let wc = "";
+
+// if (temp <= 50 && speed > 3) {
+//   wc = windChill(temp, speed);
+//   wc = `${wc}`;
+// } else {
+//   wc = "N/A";
+// }
+
+// document.querySelector("#chill").innerHTML = `${wc}&#8457`;
+
+// function windChill(temp, speed) {
+//   wc =
+//     35.74 +
+//     0.6215 * temp -
+//     35.75 * Math.pow(speed, 0.16) +
+//     0.4275 * temp * Math.pow(speed, 0.16);
+//   return Math.round(wc);
+// }
+
+// console.log(wc);
