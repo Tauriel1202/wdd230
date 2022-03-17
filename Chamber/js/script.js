2a7eebd44a231061914990a21ba7152e8a51de7f
Tauriel1202 <lothlorienexplorer20@gmail.com>
2022-03-17 15:08:22 -0400

    header+footer changes and visit js

--- Chamber/js/script.js
+++ Chamber/js/script.js
@@ -60,22 +60,6 @@ if ("IntersectionObserver" in window) {
     load(img);
   });
 }
-/**********************LAST VISIT************************/
-const visit = document.querySelector("#visit");
-const lv = Number(localStorage.getItem("visits-ls"));
-const today = Date.now();
-const msInDay = 1000 * 60 * 60 * 24;
-let timeSince = Math.round((today - lv) / msInDay);
-
-localStorage.setItem("visits-ls", today);
-
-if (visit) {
-  if (lv !== 0) {
-    visit.innerHTML = `🌊Welcome Back! 🌺 It has been ${timeSince} days since your last visit.🌴`;
-  } else {
-    visit.textContent = `🌊Welcome to the Chamber!🌺Enjoy your first visit!🌴`;
-  }
-}
 /******************Form Time**************************/
 function formTime() {
   console.clear();
